6f2ee29a368fba1b2e50b6d86e51da3f566a4fdb
K Sai Kumar <134935340+Saikumar4741@users.noreply.github.com>
2024-03-25 14:50:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let string = "";
+let memory = 0; // Initialize memory variable
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML === 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML === 'M+') {
+           
+            memory += parseFloat(string);
+        } else if (e.target.innerHTML === 'M-') {
+            
+            memory -= parseFloat(string);
+        } else {
+            console.log(e.target);
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    });
+});
